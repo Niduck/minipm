da91aec32a48bf8e47900305f0bbc7555984b12f
niduck <snicolas.du@gmail.com>
2023-12-26 21:06:16 +0100

    fix baseURl

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
-export default function({mode}){
-  const baseURL = mode === 'ghpages' ? "/dowop_time-tracking" : ""
+export default function(){
+  const baseURL = "/minipm"
 
   return defineConfig({
     base: baseURL,
