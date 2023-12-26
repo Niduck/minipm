41364c6f9ae4663aa5deb146086f878b0d379e3a
niduck <snicolas.du@gmail.com>
2023-12-26 22:10:42 +0100

    Replace BrowserRouter by HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
 import Icons from "./components/Icons.tsx";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import routes from './routing/routes.tsx'
-const router = createBrowserRouter(routes, {basename: "/minipm"})
+const router = createHashRouter(routes)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App>
